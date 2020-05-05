6118120d1e4246307b95b1b5fb3f1644ce9ea3c0
Ákos Győry <gyoryakos@gmail.com>
2020-05-05 21:07:56 +0200

    Added a new button to change the first button’s text

--- Events & Event Listeners/script.js
+++ Events & Event Listeners/script.js
@@ -1,3 +1,7 @@
-$('button').click(() => {
+$('#button-1').click(() => {
     console.log('Yeah, you clicked me');
-  })
+});
+
+$('#button-2').click(() => {
+    $('#button-1').text('Hajrá, Vasas!');
+});
