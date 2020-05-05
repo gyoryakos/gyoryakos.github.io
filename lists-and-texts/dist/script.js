d6a1b609c31381cc2cde6c75d2690bf789e7f017
Ákos Győry <gyoryakos@gmail.com>
2020-05-05 14:47:38 +0200

    Added a heading and a paragraph to html with JavaScript

--- lists-and-texts/dist/script.js
+++ lists-and-texts/dist/script.js
@@ -8,3 +8,12 @@ myNames.forEach((v) =>
   {
     $("ul").append('<li>'+ v.bold() + '</li>');
   }});
+
+  let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+  
+  $("main").append('<h1 class="k">' + additionalBlock.title + '</h1>');
+  $("main").append('<p>' + additionalBlock.text + '</p>');
+
