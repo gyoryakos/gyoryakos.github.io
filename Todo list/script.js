fa37b5cab07c8db80b40b72709d265402750f43b
Ákos Győry <gyoryakos@gmail.com>
2020-05-09 00:43:11 +0200

    Crossed the text of the actual list item by click

--- Todo list/script.js
+++ Todo list/script.js
@@ -1,4 +1,14 @@
+var counter = 1;
+
 $('#button').click(() => {
-    $('ul').append('<li>' +  document.getElementById('anything').value + '</li>');
-    $('#anything').val('');
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(document.getElementById('input').value);
+    node.appendChild(textnode);
+    document.getElementById('inputList').appendChild(node);
+    node.id = counter;
+    counter++;
+    node.addEventListener("click", function() {
+        node.style.textDecoration = "line-through";
+    });
+    $('input').val('');
 });
