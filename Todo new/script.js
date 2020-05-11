60c740f0f52dacc89cdfdb1dfd98f1b5024f5ea9
Ákos Győry <gyoryakos@gmail.com>
2020-05-11 22:18:13 +0200

    My advanced ToDo list based on another solution

---
+++ Todo new/script.js
@@ -0,0 +1,27 @@
+let refCheck = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>';
+let refTrash = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>';
+
+$("#btnTodo").on('click', addTodo);
+
+function addTodo (strtodo) {
+  if (typeof strtodo !== "string") {
+    strtodo = $("#inTodo").val();
+  }
+  if ((typeof strtodo === "string") && (strtodo != "")) {
+    $(".todoList").append("<li><div class='item'><div class='itemText'>" + strtodo + "</div>" + refTrash + refCheck + "</div></li>");
+    $("li:last-child").toggleClass("todoItem");
+    $("#inTodo").val("");
+    $("#inTodo").focus();
+  }
+}
+
+addTodo("Buy milk");
+
+$(".todoList").on("click", ".todoItem .item .fa-check-circle", (event) => {
+  $(event.target).parents("li").toggleClass("checked");
+});
+
+$(".todoList").on("dblclick", ".todoItem .item .fa-trash-alt", (event) => {
+  $(event.target).parents("li").remove();
+});
+
